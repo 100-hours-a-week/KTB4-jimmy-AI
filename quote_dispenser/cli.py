ee1aac5c470096e2ad310b1795ccd2b253aa51c7
Quart512 <jimmywon5412@gmail.com>
2026-05-17 05:26:08 +0900

    회고, 주석 정리

--- quote_dispenser/cli.py
+++ quote_dispenser/cli.py
@@ -3,17 +3,16 @@ import argparse
 
 d=base.Dispenser()
 
-
-
 parser = argparse.ArgumentParser(
                     prog='Quote Dispenser',
                     description='Can draw quotes',
-                    epilog="""
-                    python3 cli.py   :명언카드 하나 뽑기
-                    python3 cli.py n :n개 뽑기                    
-                    python3 cli.py --idx idx :인덱스로 명어카드 찾기
-                    python3 cli.py --all :전체 카탈로그 보기
-                    python3 cli.py --help    :도움말 출력
+                    epilog=
+                    """
+                    python3 cli.py   			:랜덤 명언카드 하나 뽑기
+                    python3 cli.py n 			:n개 뽑기                    
+                    python3 cli.py --idx idx 	:인덱스로 명어카드 찾기
+                    python3 cli.py --all 		:전체 카탈로그 보기
+                    python3 cli.py --help    	:도움말 출력
                     """)
 
 parser.add_argument('n', nargs='?', default=1, type=int)
